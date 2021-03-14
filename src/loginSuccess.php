2bf5865cae2014f75685d942370b2a62885d1e17
karunya30 <zcabsel@ucl.ac.uk>
2021-03-14 00:15:16 +0000

    Created foundation of sql statment for insert

--- src/loginSuccess.php
+++ src/loginSuccess.php
@@ -1,12 +1,34 @@
 <?php include 'configDB.php'; ?>
 
+<?php 
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+  $table = $_POST["table"];
+  $data = $_POST["data"];
+
+  if (!($table == "" or $data =="")){
+    
+    $table = "Coursework.".$table;
+    
+    $listOfStrings = explode(",", $data);
+    $endstring = str_repeat("?,", count($listOfStrings));
+    $endstring = substr($endstring, 0, -1);
+    $sql = "INSERT INTO ? VALUES (".$endstring.") ;";
+    echo $sql;
+  }
+}else{
+  echo "nothing";
+}
+  ?>
+
 <?php
 echo "whassup";
 echo "Click here to clean <a href = \"logout.php\" tite = \"Logout\">Session/LogOut.</a>";
 echo "<br>";
 echo "<label for=\"tables\">Choose a table to enter new info for:</label>
-
-<select name=\"cars\" id=\"cars\">
+<form class = \"form-signin\" role = \"form\" action = \"loginSuccess.php\" method = \"post\">
+<select name=\"table\" id=\"table\">
+<option disabled hidden selected>Select One</option>
   <option value=\"movies\">movies</option>
   <option value=\"tags\">tags</option>
   <option value=\"ratings\">ratings</option>
@@ -19,6 +41,11 @@ echo "<label for=\"tables\">Choose a table to enter new info for:</label>
 <br>
 Please enter information separated by commas that suit the table you have selected.<br>
 
-<label for=\"datas\">Data:</label>
-  <input type=\"text\" id=\"datas\" name=\"datas\"><br><br>";
+<label for=\"data\">Data:</label>
+  <input type=\"text\" id=\"data\" name=\"data\"><br><br>
+  
+  <button class = \"btn btn-lg btn-primary btn-block\" type = \"submit\" 
+               name = \"insert\">Add To Table</button>
+               
+               </form>";
 ?>
