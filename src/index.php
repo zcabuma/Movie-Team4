60098d882cdc7563a85c768efa9d727151340430
karunya30 <zcabsel@ucl.ac.uk>
2021-03-16 02:27:52 +0000

    Added buttons on index page to view admin and browser pages

--- src/index.php
+++ src/index.php
@@ -1,10 +1,12 @@
 
 
 <?php
+echo "Proof of load balancing <br>";
+echo "Currently being hosted on: ";
 echo gethostname(); // may output e.g,: sandie
-
+echo "<br>";
 // Or, an option that also works before PHP 5.3
-echo php_uname('n'); // may output e.g,: sandie
+//echo php_uname('n'); // may output e.g,: sandie
 ?>
 
 
@@ -39,13 +41,50 @@ echo "<br>";
 echo "<br>";
 include 'database.php'; ?>
 
-<a href="browseFilms.php">Browser Page</a>
-<?php
-echo "<br>";
-echo "<br>";
-echo "<br>";
-?>
 
-<a href="login.php">Admin Page</a>
+<head>
+
+
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>Index</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/font-awesome.min.css" rel="stylesheet">
+    <link href="css/prettyPhoto.css" rel="stylesheet">
+    <link href="css/price-range.css" rel="stylesheet">
+    <link href="css/animate.css" rel="stylesheet">
+	<link href="css/main.css" rel="stylesheet">
+	<link href="css/responsive.css" rel="stylesheet">   
+    
+    
+    
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+.button {
+  background-color: #4CAF50; /* Green */
+  border: none;
+  color: white;
+  padding: 50px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 4px 2px;
+  cursor: pointer;
+  border-radius: 4px;
+  
+}
+
+</style>
+    
+</head><!--/head-->
+
+<button  class ="button" style = "float: left;margin-left: 250px " ><a href="browseFilms.php">Browser Page</a></button>
+
+
+<button  class ="button"style="float: right;margin-right: 250px;"><a href="login.php">Admin Page</a></button>
 
 
